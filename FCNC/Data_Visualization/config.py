cbb28eac6cd4e70935f2603a2a5d48a8f0d3fecc
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2021-06-08 15:48:48 +0100

    Add files via upload

---
+++ FCNC/Data_Visualization/config.py
@@ -0,0 +1,2 @@
+
+Archive_Path = r'C:\Users\migue\Desktop\estagio_lip_temp\Archive'
---
+++ Monotop/ML/config.py
@@ -0,0 +1,6 @@
+
+# Windows
+# Archive_Path = r'C:\Users\migue\Desktop\estagio_lip_temp\Archive'
+
+# Ubuntu
+Archive_Path = r'/mnt/c/Users/migue/Desktop/estagio_lip/Monotop/Archive'
